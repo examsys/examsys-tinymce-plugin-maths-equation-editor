5011ec6ce71efbe0281197246b39bb43648c9e31
Neill Magill <neill.magill@nottingham.ac.uk>
2025-07-01 15:08:29 +0100

    EXAMSYS-3489 htmlentities requires string values
    This script is to fix an issue found by Rector.
    
    The htmlentities method requires it's input to be a string.
    
    We cannot guarantee that all the posted values will be so we need to cast them.
    
    This will prevent potential notices under PHP 8.1 and above.

--- mee/tinymce_res.php
+++ mee/tinymce_res.php
@@ -18,6 +18,6 @@
 echo '<pre>';
 foreach ($_POST as $var => $value) {
     echo "$var => \n";
-    echo htmlentities($value) . "\n";
+    echo htmlentities((string) $value) . "\n";
 }
 echo '</pre>';
